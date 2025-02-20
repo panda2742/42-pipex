24826060d3482a5a8400cac22f8f4a3efd42c622
elouannh <elouannhosta27@gmail.com>
2025-02-20 19:59:29 +0100

    Pushing fixed pipex

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ehosta <ehosta@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 17:59:49 by ehosta            #+#    #+#             */
-/*   Updated: 2025/02/20 18:47:44 by ehosta           ###   ########.fr       */
+/*   Updated: 2025/02/20 19:59:09 by ehosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include <sys/wait.h>
 # include <string.h>
 # include <stdio.h>
-# include "../libft/include/libft.h"
+# include "../lib/include/libft.h"
 
 # define ERR_INFILE "Infile"
 # define ERR_OUTFILE "Outfile"
